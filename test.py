ac88596c13c0565ea9061423f3cd6056a1474a09
IgorAlexeew <goscha.alekseew2015@yandex.ru>
2020-07-20 19:52:32 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-print('Hello, World!')
-print('It is test file.')
-print('I hope it is working...')
-print('Мда...')
+# print('Hello, World!')
+# print('It is test file.')
+# print('I hope it is working...')
+# print('Мда...')
+
+import time
+
+def logsmth(text):
+    # text_array = []
+    # text_array = ' '.join(args)
+    with open('mysite/log_by_hook.txt', 'a') as f:
+        f.write(time.strftime("%H:%M:%S %d.%m.%Y", time.localtime()) + f' -> {text}' + '\n')
