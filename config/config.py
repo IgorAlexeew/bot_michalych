76fcb59ffed5ce576d7105db141e955e0088166f
IgorAlexeew <goscha.alekseew2015@yandex.ru>
2020-08-02 20:45:58 +0400

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ import datetime
 def get_path():
     sep = '\\' if '\\' in __file__ else '/'
     path_arr = __file__.split(sep)
-    path_str = sep.join(path[:-1] + [''])
+    path_str = sep.join(path_arr[:-1] + [''])
 
     return path_str, path_arr, sep
 
